G3-of-Java-for-MDT-in-FHDo-24WS/capstone_project

commit cf22a8461b535d832614e31490efd0a782840f7a

feat: Replace Console UI with GUI

src/main/java/de/fhdo/service/SystemMonitor.java
@@ -16,7 +16,6 @@ public class SystemMonitor {
     private static SystemMonitor instance;
 
     private SystemMonitor() {
-        startMonitoring();
     }
 
     public static synchronized SystemMonitor getInstance() {
@@ -26,9 +25,9 @@ public class SystemMonitor {
         return instance;
     }
 
-    private void startMonitoring() {
-        scheduler.scheduleAtFixedRate(this::monitorSystemStatus, 0, 5, TimeUnit.SECONDS);
-        scheduler.scheduleAtFixedRate(this::logSystemData, 0, 5, TimeUnit.SECONDS);
+    public void startMonitoring() {
+        scheduler.scheduleAtFixedRate(this::monitorSystemStatus, 0, 1, TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(this::logSystemData, 0, 1, TimeUnit.SECONDS);
     }
 
     private void monitorSystemStatus() {
@@ -62,7 +61,7 @@ public class SystemMonitor {
     public void shutdown() {
         scheduler.shutdown();
         try {
-            if (!scheduler.awaitTermination(60, TimeUnit.SECONDS)) {
+            if (!scheduler.awaitTermination(5, TimeUnit.SECONDS)) {
                 scheduler.shutdownNow();
             }
         } catch (InterruptedException e) {

src/test/java/de/fhdo/service/SystemMonitorTest.java
@@ -30,11 +30,6 @@ public class SystemMonitorTest {
         energyManager.clearAllBatteries();
     }
 
-//    @AfterEach
-//    void tearDown() {
-//        systemMonitor.shutdown();
-//    }
-
     @Test
     void testGetInstance() {
         SystemMonitor instance1 = SystemMonitor.getInstance();

src/main/java/de/fhdo/App.java
@@ -1,16 +1,20 @@
 package de.fhdo;
 
-import de.fhdo.ui.MainMenu;
+import de.fhdo.gui.MainFrame;
 import de.fhdo.service.SystemMonitor;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.swing.*;
 
 @Slf4j
 public class App {
     public static void main(String[] args) {
         SystemMonitor monitor = SystemMonitor.getInstance();
-        MainMenu mainMenu = new MainMenu();
+        monitor.startMonitoring();
 
-        mainMenu.show();
-        monitor.shutdown();
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
     }
 }

src/main/java/de/fhdo/model/Battery.java
@@ -3,8 +3,6 @@ package de.fhdo.model;
 import lombok.Data;
 import lombok.Builder;
 
-import java.util.Objects;
-
 @Data
 @Builder
 public class Battery {

src/main/java/de/fhdo/model/Device.java
@@ -3,8 +3,6 @@ package de.fhdo.model;
 import lombok.Data;
 import lombok.Builder;
 
-import java.util.Objects;
-
 @Data
 @Builder
 public class Device {

src/main/java/de/fhdo/model/Energy.java
@@ -3,8 +3,6 @@ package de.fhdo.model;
 import lombok.Data;
 import lombok.Builder;
 
-import java.util.Objects;
-
 @Data
 @Builder
 public class Energy {

src/main/java/de/fhdo/service/DeviceManager.java
@@ -33,6 +33,7 @@ public class DeviceManager {
     public void addDevice(Device device) {
         devices.put(device.getId(), device);
         LoggerHelper.logDeviceEvent(logManager, "Added new device", device.getName());
+        log.info("Device added: {}", device);
     }
 
     public void removeDevice(String deviceId) {

src/main/java/de/fhdo/service/EnergyManager.java
@@ -50,11 +50,13 @@ public class EnergyManager {
     public void addEnergy(Energy energy) {
         energies.put(energy.getId(), energy);
         LoggerHelper.logEnergyEvent(logManager, "Added new energy", energy.getName());
+        log.info("Energy added: {}", energy);
     }
 
     public void addBattery(Battery battery) {
         batteries.put(battery.getId(), battery);
         LoggerHelper.logBatteryEvent(logManager, "Added new battery", battery.getName());
+        log.info("Battery added: {}", battery);
     }
 
     public Battery getBatteryById(String batteryId) {
@@ -173,7 +175,7 @@ public class EnergyManager {
                     break;
                 }
 
-                Thread.sleep(3000);
+                Thread.sleep(2000);
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -211,7 +213,7 @@ public class EnergyManager {
                     }
                 }
 
-                Thread.sleep(3000);
+                Thread.sleep(2000);
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();

commit 15a92211943b293ab899142aba8865eec6a0e0bc

fix: Fix testing issues caused by cf22a846

src/main/java/de/fhdo/service/SystemMonitor.java
@@ -15,6 +15,8 @@ public class SystemMonitor {
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
     private static SystemMonitor instance;
 
+    private boolean isMonitoring = false;
+
     private SystemMonitor() {
     }
 
@@ -26,6 +28,14 @@ public class SystemMonitor {
     }
 
     public void startMonitoring() {
+        if (isMonitoring) {
+            log.warn("System monitor is already running");
+            return;
+        } else {
+            log.info("Starting system monitor");
+            isMonitoring = true;
+        }
+
         scheduler.scheduleAtFixedRate(this::monitorSystemStatus, 0, 1, TimeUnit.SECONDS);
         scheduler.scheduleAtFixedRate(this::logSystemData, 0, 1, TimeUnit.SECONDS);
     }

src/test/java/de/fhdo/config/HouseConfigTest.java
@@ -25,13 +25,13 @@ public class HouseConfigTest {
         HouseConfig.EnergyConfig firstEnergy = config.getEnergies().get(0);
         assertEquals("Solar Panels", firstEnergy.getName());
         assertEquals("SOLAR", firstEnergy.getType());
-        assertEquals(3000.0, firstEnergy.getOutput());
+        assertEquals(1500, firstEnergy.getOutput());
         
         assertEquals(1, config.getBatteries().size());
         HouseConfig.BatteryConfig battery = config.getBatteries().get(0);
         assertEquals("Main Battery", battery.getName());
         assertEquals(10000.0, battery.getCapacity());
-        assertEquals(2000.0, battery.getMaxChargeRate());
+        assertEquals(1000.0, battery.getMaxChargeRate());
     }
     
     @Test

src/test/java/de/fhdo/service/SystemMonitorTest.java
@@ -2,6 +2,7 @@ package de.fhdo.service;
 
 import de.fhdo.model.Device;
 import de.fhdo.model.Battery;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -28,6 +29,8 @@ public class SystemMonitorTest {
         deviceManager.clearAllDevices();
         energyManager.clearAllEnergies();
         energyManager.clearAllBatteries();
+
+        systemMonitor.startMonitoring();
     }
 
     @Test